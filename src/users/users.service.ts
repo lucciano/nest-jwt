05f4cbeb2992654ca27497939962186140af704f
Luciano Andrade <andrade.luciano@gmail.com>
2022-05-24 20:50:35 -0300

    local auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'admin',
+      password: 'admin',
+    },
+    {
+      userId: 2,
+      username: 'user',
+      password: 'user',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
